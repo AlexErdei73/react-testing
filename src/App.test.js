dda7028220f7792e5bf11009f7dbef22fc24598c
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-13 21:36:17 +0100

    First test: simplified version

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("App component", () => {
-	it("renders correct heading", () => {
-		render(<App />);
-		expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
-	});
+test("renders correct heading", () => {
+	render(<App />);
+	expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
 });
